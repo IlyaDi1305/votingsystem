21e7f04c5002f0d085e9352cc8234e5f54c8d5c4
admin <didorenko1305@gmail.com>
2024-12-29 02:46:46 +0100

    deleting CorsConfigurationSource in SecurityConfig,
    pom: spring-boot-starter-parent 3.4.1,
    ch.qos.logback 1.5.15 (2 vulnerabilities 1.5.12)

--- src/main/java/ru/didorenko/votingsystem/app/config/SecurityConfig.java
+++ src/main/java/ru/didorenko/votingsystem/app/config/SecurityConfig.java
@@ -14,15 +14,10 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import ru.didorenko.votingsystem.app.AuthUser;
 import ru.didorenko.votingsystem.model.Role;
 import ru.didorenko.votingsystem.model.User;
 import ru.didorenko.votingsystem.repository.UserRepository;
-
-import java.util.List;
 import java.util.Optional;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -64,17 +59,5 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(withDefaults());
         return http.build();
-    }
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://restvoting.kesug.com"));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
-        configuration.setAllowCredentials(true);
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
     }
 }
